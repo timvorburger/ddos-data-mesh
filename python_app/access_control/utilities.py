e85d8ea5a2db490e6c2647413fdc4d4cf02662e0
timvorburger <tim.vorburger@uzh.ch>
2024-03-24 23:08:31 -0600

    code clean up and updated folder structure

--- python_app/access_control/utilities.py
+++ python_app/access_control/utilities.py
@@ -4,7 +4,6 @@ def read_json_file(file_path):
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
-            #print(f"Data from {file_path}:\n{json.dumps(data, indent=4)}\n")
             return data
     except FileNotFoundError:
         print(f"File {file_path} not found.")
@@ -16,7 +15,6 @@ def write_json_file(file_path, data):
     try:
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=4)
-            #print(f"Data written to {file_path}\n")
     except FileNotFoundError:
         print(f"File {file_path} not found.")
     except Exception as e:
